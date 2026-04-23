4d2d7a36e44b1dedbb2d33f343f8e7cb3f7bafeb
Artem Yakhno <artem.yakhno.ua@gmail.com>
2026-04-23 15:20:29 +0300

    fixs: check falback secret

--- src/configs/jwt.confing.ts
+++ src/configs/jwt.confing.ts
@@ -5,8 +5,17 @@ import { JwtModuleOptions } from '@nestjs/jwt';
 export async function getJwtConfig(
   configService: ConfigService,
 ): Promise<JwtModuleOptions> {
+  const secret =
+    configService.get<string>('JWT_SECRET') || process.env.JWT_SECRET; // ← fallback напряму до process.env
+
+  if (!secret) {
+    throw new Error(
+      `JWT_SECRET is undefined! All env keys: ${Object.keys(process.env).join(', ')}`,
+    );
+  }
+
   return {
-    secret: configService.getOrThrow<string>('JWT_SECRET'),
+    secret: secret,
     signOptions: {
       algorithm: 'HS256',
     },
